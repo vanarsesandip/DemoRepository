02000bcf4fc853ab498e22bd1a7e6d0140a45f00
vanarsesandip <vanarse.sandip2620@gmail.com>
2023-02-20 09:02:43 +0530

    Changes Successfully

--- Student.java
+++ Student.java
@@ -1,5 +1,12 @@
 public class Student {
 
+
+    public void mul(){
+         int a = 10;
+        int b = 20;
+        int mul = a * b;
+        System.out.println(mul);
+    }
     public static void main(String[] args) {
         int a = 10;
         int b = 20;

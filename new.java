3669924df7c6320bee986eb7a94f8fdba8292349
vanarsesandip <vanarse.sandip2620@gmail.com>
2023-02-24 08:53:43 +0530

    New File updated

--- new.java
+++ new.java
@@ -1,7 +1,10 @@
 public class new{
 
     public static void main(String[] args) {
+    int a=1;
+    int b=20;
+    int sum=a+b;
         
-        System.out.println("Hi Git");
+        System.out.println("sum");
     }
 }
